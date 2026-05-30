6f3429e5880c378ed77e1f040edb0ad59cef6768
Daniel <ejsday@gmail.com>
2026-05-30 22:32:45 +0900

    요금 수정

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get("/distance", async (req, res) => {
     const toll = summary.tollFare;
 
     // 예상 택시비
-    const price = Math.round(distance * 1400);
+    const price = Math.round(distance * 1500);
 
     res.json({
       distance,
